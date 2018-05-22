2b92e8844a0cadcb02d3b73b17af22444949e48d
josef <josef.aidt@gmail.com>
2018-05-21 19:48:51 -0500

    added gulp to strip comments from json

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const gulp = require('gulp')
+const strip = require('gulp-strip-comments')
+ 
+gulp.task('default', () => {
+  return gulp.src('src/themes/*.json')
+    .pipe(strip())
+    .pipe(gulp.dest('themes/'))
+});

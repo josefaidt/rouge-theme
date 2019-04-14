07c6e4fa382da45ee88f24b63384b78cb6a7809d
josef <josef.aidt@gmail.com>
2019-04-14 15:12:32 -0500

    add input, widget, scrollbar

--- src/rouge2/editorColors/index.js
+++ src/rouge2/editorColors/index.js
@@ -5,11 +5,7 @@ const { augment } = theme
 
 module.exports = {
   focusBorder: theme.bg_lightest,
-  foreground: theme.bg_lightest,
-  'widget.shadow': theme.bg_dark,
-  'selection.background': '#fff',
-  descriptionForeground: '#fff',
-  errorForeground: '#fff',
+  foreground: theme.bg_lighter,
   'editor.background': theme.bg,
   'sideBar.background': theme.bg,
   'editorGroupHeader.noTabsBackground': theme.bg_darker,
@@ -18,7 +14,7 @@ module.exports = {
   //
   // ACTIVITY BAR
   //
-  'activityBar.background': theme.bg_dark,
+  'activityBar.background': theme.bg,
   'activityBar.foreground': theme.bg_lighter,
   'activityBarBadge.background': theme.rouge,
   'activityBarBadge.foreground': theme.rouge_lightest,
@@ -27,7 +23,7 @@ module.exports = {
   // SIDEBAR
   //
   'sideBar.background': theme.bg,
-  'sideBar.border': theme.bg_Dark,
+  'sideBar.border': theme.bg_dark,
   'sideBar.dropBackground': theme.rouge,
   'sideBar.foreground': theme.bg_lighter,
   'sideBarSectionHeader.background': theme.bg,
@@ -42,25 +38,25 @@ module.exports = {
   'tab.inactiveBackground': theme.bg,
   'tab.inactiveForeground': theme.bg_lighter,
   'tab.border': theme.bg_dark,
-  'tab.activeBorder': theme.rouge_light,
+  // 'tab.activeBorder': augment(theme.rouge, 85),
   // 'tab.activeBorderTop': theme.rouge,
   // 'tab.hoverBackground': augment(theme.bg_light, 10),
-  'tab.hoverBorder': theme.rouge_darker,
+  // 'tab.hoverBorder': theme.rouge_darker,
   // 'tab.activeModifiedBorder': theme.rouge_lightest,
   // 'tab.inactiveModifiedBorder': theme.rouge_lightest,
   'tab.unfocusedInactiveForeground': theme.bg_light,
-  'tab.unfocusedActiveForeground': theme.bg_lighter,
-  'tab.unfocusedActiveBorder': theme.rouge_darker,
+  'tab.unfocusedActiveForeground': augment(theme.bg_lightest, 70),
+  // 'tab.unfocusedActiveBorder': theme.rouge_darker,
   // 'tab.unfocusedActiveBorderTop': theme.rouge_darker,
   // 'tab.unfocusedHoverBackground': augment(theme.bg_light, 10),
-  'tab.unfocusedHoverBorder': theme.rouge_darker,
+  // 'tab.unfocusedHoverBorder': theme.rouge_darker,
   // 'tab.unfocusedActiveModifiedBorder': theme.rouge_lightest,
   // 'tab.unfocusedInactiveModifiedBorder': theme.rouge_lightest,
-  'editorPane.background': '#fff',
+  'editorPane.background': '#f3f',
   //
   // LIST
   //
-  'list.activeSelectionBackground': theme.rouge_dark,
+  'list.activeSelectionBackground': augment(theme.rouge, 85),
   'list.activeSelectionForeground': theme.rouge_lightest,
   'list.dropBackground': '#75809566',
   'list.focusBackground': '#293a57',
@@ -70,12 +66,54 @@ module.exports = {
   'list.inactiveFocusBackground': theme.bg_dark,
   'list.inactiveFocusForeground': theme.bg_light,
   'list.inactiveSelectionBackground': augment(theme.bg_light, 30),
-  'list.inactiveSelectionForeground': theme.bg_lightest,
+  'list.inactiveSelectionForeground': augment(theme.bg_lightest, 90),
   'list.invalidItemForeground': '#CC565E',
   'list.errorForeground': '#CC565E',
   'list.warningForeground': '#F9B5ACcc',
   'listFilterWidget.background': theme.bg,
   'listFilterWidget.outline': theme.rouge,
   'listFilterWidget.noMatchesOutline': theme.rouge_darker,
+  //
+  // WIDGETS
+  //
+  'widget.shadow': theme.bg_darker,
+  'editorWidget.background': augment(theme.bg_light, 50),
+  'editorWidget.background': augment(theme.bg_light, 20),
+  'editorWidget.border': '#c6797e',
+  'editorWidget.resizeBorder': theme.rouge,
+  'editorSuggestWidget.background': '#1F2A3F',
+  'editorSuggestWidget.border': '#758095',
+  'editorSuggestWidget.foreground': '#c6cad2',
+  'editorSuggestWidget.highlightForeground': '#eabe9a',
+  'editorSuggestWidget.selectedBackground': '#293a57',
+  'editorHoverWidget.background': '#1F2A3F',
+  'editorHoverWidget.border': '#758095',
+  'selection.background': '#f3f',
+  descriptionForeground: '#f3f',
+  errorForeground: '#f3f',
+  //
+  // INPUT
+  //
+  'input.background': theme.bg,
+  'input.border': theme.rouge_dark,
+  'input.foreground': theme.bg_lightest,
+  'inputOption.activeBorder': '#b8bcc6',
+  'input.placeholderForeground': '#808799',
+  'inputValidation.errorBackground': theme.bg,
+  'inputValidation.errorBorder': '#CC565Edf',
+  'inputValidation.warningBackground': theme.bg,
+  'inputValidation.warningBorder': '#eabe9adf',
+  'inputValidation.infoBackground': theme.bg,
+  'inputValidation.infoBorder': '#6e94b999',
+  //
+  // SCROLLBAR
+  //
+  'scrollbar.shadow': augment(theme.rouge_darker, 10),
+  'scrollbarSlider.activeBackground': augment(theme.rouge, 75),
+  'scrollbarSlider.background': augment(theme.rouge_dark, 60),
+  'scrollbarSlider.hoverBackground': augment(theme.rouge, 60),
+  //
+  // TERMINAL COLORS
+  //
   ...terminalColors,
 }

cc20310ecb6eac5baccc1f2bcbef2dace90bf527
josef <josef.aidt@gmail.com>
2020-01-08 20:42:36 -0600

    alpha.24 - adjust terminal, debug icons

--- src/rouge2/editorColors/terminal/index.js
+++ src/rouge2/editorColors/terminal/index.js
@@ -1,4 +1,5 @@
 const theme = require('../../theme')
+const { opac } = require('../../theme')
 module.exports = {
   'terminal.background': theme.bg,
   'terminal.foreground': theme.bg_lighter,
@@ -9,15 +10,15 @@ module.exports = {
   'terminal.ansiCyan': '#8ab6c1',
   'terminal.ansiGreen': theme.green,
   'terminal.ansiMagenta': theme.purple,
-  'terminal.ansiRed': theme.rouge,
+  'terminal.ansiRed': theme.rouge_darker,
   'terminal.ansiWhite': theme.bg_lightest,
-  'terminal.ansiYellow': theme.grapple,
-  'terminal.ansiBrightBlack': theme.bg_lighter,
+  'terminal.ansiYellow': opac(theme.grapple, 90),
+  'terminal.ansiBrightBlack': theme.bgb_light,
   'terminal.ansiBrightBlue': '#98b3cd',
   'terminal.ansiBrightCyan': '#abcbd3',
   'terminal.ansiBrightGreen': theme.green_light,
   'terminal.ansiBrightMagenta': theme.purple_light,
-  'terminal.ansiBrightRed': theme.rouge_light,
+  'terminal.ansiBrightRed': theme.rouge,
   'terminal.ansiBrightWhite': theme.bg_lightest,
-  'terminal.ansiBrightYellow': theme.grapple_light,
+  'terminal.ansiBrightYellow': theme.grapple,
 }

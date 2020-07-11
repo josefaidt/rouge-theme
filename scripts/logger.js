244e866787ee5f797601777c56865f09253c87c8
josefaidt <josef.aidt@gmail.com>
2020-07-11 10:05:40 -0500

    chore: modify launch tasks, prebuild, clean up package.json

--- scripts/logger.js
+++
@@ -1,9 +0,0 @@
-const chalk = require('chalk')
-
-const logger = phrase => {
-  const d = new Date()
-  const timeStr = `[${d.toTimeString().split(' ')[0]}]`
-  return `${chalk.gray(timeStr)} ${phrase || ''}`
-}
-
-module.exports = logger
